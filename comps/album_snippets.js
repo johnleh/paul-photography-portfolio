81881929ff1afb01cf7ba48ab5a712cf5adc3b42
johnleh <johnleh@live.unc.edu>
2022-03-19 16:21:02 -0400

    comment

--- comps/album_snippets.js
+++ comps/album_snippets.js
@@ -2,7 +2,9 @@ import Link from "next/link";
 import {album_data} from '../public/images/album_data.js'
 import React from 'react'
 
-
+/*
+  AlbumSnippets are the cards that show a preview of a photo album
+*/
 class AlbumSnippet extends React.Component {
   render() {
     return (
@@ -16,6 +18,7 @@ class AlbumSnippet extends React.Component {
                   <a className="album_snippet_button">Explore all <i class="arrow_right"></i></a>
               </Link>
             </h2>
+            {/*Actual images, pics the first 3 of the album [only one right now...]*/}
             <div className="album_snippet">              
               <table className="album_snippet_table">
                 <tr>
@@ -46,15 +49,4 @@ class AlbumSnippet extends React.Component {
 }
 export default AlbumSnippet
 
-/*
-{album_data.map(function (album) {
-            return (
-              <div>
-                <Link href={'/albums/' + album.id} key={album.id}>
-                    <a>{album.name}</a>
-                </Link>
-              </div>
-            )
-          })}
-    
-*/
+
